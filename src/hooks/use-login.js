494fcab32eebe7e3b9b7b890cac799b504a73868
Johannes Hofmockel <johannes.hofmockel@lavita.de>
2022-05-20 08:54:41 +0200

    format code

--- src/hooks/use-login.js
+++ src/hooks/use-login.js
@@ -2,12 +2,12 @@ import { API } from "@lavita-io/bizberry-sdk"
 import { useMutation } from "react-query"
 
 /**
- * 
- * @param {API} client 
- * @returns 
+ *
+ * @param {API} client
+ * @returns
  */
-const useLogin = (client) => {
-    return useMutation(async (values) => await client.login(values))
+const useLogin = client => {
+    return useMutation(async values => await client.login(values))
 }
 
 export { useLogin }
