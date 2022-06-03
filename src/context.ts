fe4c5ed31a1678b11354bd5fce10b358c4fa7066
Johannes Hofmockel <johannes.hofmockel@lavita.de>
2022-06-03 09:12:27 +0200

    feat: add new API as hook

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,4 @@
-import React, { createContext } from "react"
+import React, { createContext, useContext } from "react"
 
 import { API } from "./api"
 import { store } from "@lavita-io/bizberry-sdk"
