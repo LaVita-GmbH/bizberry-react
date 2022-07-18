5cd61b1a13f28c3c09080accfb37d6e82f781371
Johannes Hofmockel <johannes.hofmockel@lavita.de>
2022-07-18 12:10:54 +0200

    fix: add mutation keys for login

--- src/hooks/use-login.ts
+++ src/hooks/use-login.ts
@@ -3,7 +3,7 @@ import { useMutation } from "react-query"
 
 const useLogin = () => {
     const client = useAPI()
-    return useMutation(async values => await client.login(values))
+    return useMutation(async values => await client.login(values), { mutationKey: ["bizberry-react", "access", "auth", "user"]})
 }
 
 export { useLogin }
